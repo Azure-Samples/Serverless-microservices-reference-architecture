d5bdd495db23ea6495feaa3db243f6f297321ec8
Gerardo Melendrez <payini@hotmail.com>
2018-08-28 03:01:47 -0700

    Configure the APIs within the SPA to use APIM base API addresses and to use the APIM access token

--- web/serverless-microservices-web/src/api/drivers.js
+++ web/serverless-microservices-web/src/api/drivers.js
@@ -1,45 +1,46 @@
-import { checkResponse, post, get, put } from '@/utils/http';
+import { checkResponse, post, get, put, getApi, postApi, putApi } from '@/utils/http';
 //const baseUrl = 'http://localhost:7071/api';
 const baseUrl = window.apiDriversBaseUrl;
 const code = window.apiDriversCode;
+const apiKey = window.apiKey;
 
 // GET methods
-export function getDrivers() {
-  return get(`${baseUrl}/drivers?${code}`).then(checkResponse);
+export function getDrivers () {
+  return getApi(`${baseUrl}/drivers?${code}`, {}, apiKey).then(checkResponse);
 }
 
-export function getDriver(driverCode) {
-  return get(
-    `${baseUrl}/drivers/${driverCode}?code=gq9IhFf24ywXYsiAVpr90KtEyXBfDpubeOT3RJvbIpTSda/FAPN3ug==`
+export function getDriver (driverCode) {
+  return getApi(
+    `${baseUrl}/drivers/${driverCode}?code=gq9IhFf24ywXYsiAVpr90KtEyXBfDpubeOT3RJvbIpTSda/FAPN3ug==`, {}, apiKey
   ).then(checkResponse);
 }
 
-export function getActiveDrivers() {
-  return get(`${baseUrl}/activedrivers?${code}`).then(checkResponse);
+export function getActiveDrivers () {
+  return getApi(`${baseUrl}/activedrivers?${code}`, {}, apiKey).then(checkResponse);
 }
 
-export function getDriversWithinLocation(latitude, longitude, miles) {
-  return get(
-    `${baseUrl}/drivers/${latitude}/${longitude}/${miles}?${code}`
+export function getDriversWithinLocation (latitude, longitude, miles) {
+  return getApi(
+    `${baseUrl}/drivers/${latitude}/${longitude}/${miles}?${code}`, {}, apiKey
   ).then(checkResponse);
 }
 
-export function getDriverLocationChanges(driverCode) {
-  return get(`${baseUrl}/driverlocations/${driverCode}?${code}`).then(
+export function getDriverLocationChanges (driverCode) {
+  return getApi(`${baseUrl}/driverlocations/${driverCode}?${code}`, {}, apiKey).then(
     checkResponse
   );
 }
 
 // POST methods
-export function createDriver(driver) {
-  return post(`${baseUrl}/drivers?${code}`, driver).then(checkResponse);
+export function createDriver (driver) {
+  return postApi(`${baseUrl}/drivers?${code}`, {}, apiKey, driver).then(checkResponse);
 }
 
 // PUT methods
-export function updateDriver(driver) {
-  return put(`${baseUrl}/drivers?${code}`, driver).then(checkResponse);
+export function updateDriver (driver) {
+  return putApi(`${baseUrl}/drivers?${code}`, {}, apiKey, driver).then(checkResponse);
 }
 
-export function updateDriverLocation(driver) {
-  return put(`${baseUrl}/driverlocations?${code}`, driver).then(checkResponse);
+export function updateDriverLocation (driver) {
+  return putApi(`${baseUrl}/driverlocations?${code}`, {}, apiKey, driver).then(checkResponse);
 }
