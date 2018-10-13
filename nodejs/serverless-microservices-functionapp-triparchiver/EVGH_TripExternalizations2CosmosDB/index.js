a897082acdfce64821fc5f03db299c0529eb5c27
Joel Hulen <joelhulen@gmail.com>
2018-10-13 14:44:02 -0400

    Updated Trip Archiver function

--- nodejs/serverless-microservices-functionapp-triparchiver/EVGH_TripExternalizations2CosmosDB/index.js
+++ nodejs/serverless-microservices-functionapp-triparchiver/EVGH_TripExternalizations2CosmosDB/index.js
@@ -1,13 +1,15 @@
-module.exports = function (context, eventGridEvent) {
-    context.log(typeof eventGridEvent);
-    context.log(eventGridEvent);
+module.exports = function(context, eventGridEvent) {
+  context.log(typeof eventGridEvent);
+  context.log(eventGridEvent);
 
-    context.log("JavaScript Event Grid function processed a request.");
-    context.log("Subject: " + eventGridEvent.subject);
-    context.log("Time: " + eventGridEvent.eventTime);
-    context.log("Data: " + JSON.stringify(eventGridEvent.data));
+  context.log(
+    'EVGH_TripExternalizataions2CosmosDB triggered....EventGridEvent'
+  );
+  context.log('Subject: ' + eventGridEvent.subject);
+  context.log('Time: ' + eventGridEvent.eventTime);
+  context.log('Data: ' + JSON.stringify(eventGridEvent.data));
 
-    context.bindings.document = JSON.stringify(eventGridEvent.data);
+  context.bindings.document = JSON.stringify(eventGridEvent.data);
 
-    context.done();
+  context.done();
 };
