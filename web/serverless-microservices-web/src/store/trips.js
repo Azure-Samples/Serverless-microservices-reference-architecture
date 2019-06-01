936f9d121afb7620d1734a1f47b99bf3e30b7d9a
Joel Hulen <joelhulen@gmail.com>
2019-06-01 17:31:34 -0400

    Fixed formatting

--- web/serverless-microservices-web/src/store/trips.js
+++ web/serverless-microservices-web/src/store/trips.js
@@ -1,15 +1,15 @@
-import { createTrip, getSignalRInfo, submitTripReview } from '@/api/trips';
+import { createTrip, getSignalRInfo, submitTripReview } from '@/api/trips'
 
 export default {
   namespaced: true,
 
-  state() {
+  state () {
     return {
       trip: {},
       currentStep: 0,
       reviewResults: {},
       contentLoading: false
-    };
+    }
   },
 
   getters: {
@@ -20,73 +20,74 @@ export default {
   },
 
   mutations: {
-    trip(state, value) {
-      state.trip = value;
+    trip (state, value) {
+      state.trip = value
     },
-    currentStep(state, value) {
-      state.currentStep = value;
+    currentStep (state, value) {
+      state.currentStep = value
     },
-    reviewResults(state, value) {
-      state.reviewResults = value;
+    reviewResults (state, value) {
+      state.reviewResults = value
     },
-    contentLoading(state, value) {
-      state.contentLoading = value;
+    contentLoading (state, value) {
+      state.contentLoading = value
     }
   },
 
   actions: {
-    setTrip({ commit }, value) {
-      commit('trip', value);
+    setTrip ({ commit }, value) {
+      commit('trip', value)
     },
 
-    setCurrentStep({ commit }, value) {
-      commit('currentStep', value);
+    setCurrentStep ({ commit }, value) {
+      commit('currentStep', value)
     },
 
-    clearReviewResults({ commit }) {
-      commit('reviewResults', {});
+    clearReviewResults ({ commit }) {
+      commit('reviewResults', {})
     },
 
-    async createTrip({ commit }, value) {
+    async createTrip ({ commit }, value) {
       try {
-        commit('contentLoading', true);
-        let trip = await createTrip(value);
-        commit('trip', trip.data);
-        return trip.data;
+        commit('contentLoading', true)
+        let trip = await createTrip(value)
+        commit('trip', trip.data)
+        return trip.data
       } catch (e) {
-        throw e;
+        throw e
       } finally {
-        commit('contentLoading', false);
+        commit('contentLoading', false)
       }
     },
 
-    async submitTripReview({ commit }, payload) {
+    async submitTripReview ({ commit }, payload) {
       try {
-        commit('contentLoading', true);
+        commit('contentLoading', true)
         let value = {
           driverRating: payload.rating,
           review: payload.review
-        };
+        }
         let results = await submitTripReview(
           payload.code,
           payload.driverCode,
           value
-        );
-        commit('reviewResults', results.data);
-        return results.data;
+        )
+        commit('reviewResults', results.data)
+        return results.data
       } catch (e) {
-        throw e;
+        throw e
       } finally {
-        commit('contentLoading', false);
+        commit('contentLoading', false)
       }
     },
 
-    async getSignalRInfo({ commit }, username) {
+    async getSignalRInfo ({ commit }, username) {
       try {
-        let signalRInfo = await getSignalRInfo(username);
-        return signalRInfo;
+        let signalRInfo = await getSignalRInfo(username)
+        return signalRInfo
       } catch (e) {
-        throw e;
+        throw e
       }
     }
-};
+  }
+}
