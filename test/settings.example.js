6d929b79d0c762d6c27eda3afaa40cb42f9d0c1b
Daniel Larsen <dalars@microsoft.com>
2021-11-25 10:33:27 +1300

    Local SPA

---
+++ test/settings.example.js
@@ -0,0 +1,13 @@
+// Auth
+window.authClientId = '';
+window.authAuthority = '';
+window.authScopes = [''];
+window.authEnabled = false;
+
+// API endpoints
+window.apiKey = '';
+window.apiBaseUrl = '';
+window.apiDriversBaseUrl = `http://localhost:7071/api`;
+window.apiTripsBaseUrl = `http://localhost:7072/api`;
+window.apiPassengersBaseUrl = `http://localhost:7073/api`;
+window.signalrInfoUrl = '';
