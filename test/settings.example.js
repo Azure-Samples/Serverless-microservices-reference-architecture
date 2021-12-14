3e32b0ed216aaa87612db74476286ac64b9b1920
Victor Viriya-ampanond <15903177+wviriya@users.noreply.github.com>
2021-12-14 14:34:35 +1300

    Update getToken function and create a separate application with a new custom scope

--- test/settings.example.js
+++ test/settings.example.js
@@ -3,8 +3,8 @@ window.authClientId = 'a298b04d-dbe2-4fc8-8191-b64c1fd287e2';
 window.authAuthority = 'https://wviriyab2c.b2clogin.com/wviriyab2c.onmicrosoft.com/B2C_1_default-signin';
 window.knownAuthority = 'wviriyab2c.b2clogin.com';
 window.redirectUri = 'http://localhost:4280';
-window.loginScopes = ['https://wviriyab2c.onmicrosoft.com/a298b04d-dbe2-4fc8-8191-b64c1fd287e2/rideshare'];
-window.apiScopes = ['https://wviriyab2c.onmicrosoft.com/a298b04d-dbe2-4fc8-8191-b64c1fd287e2/rideshare'];
+window.loginScopes = ['https://wviriyab2c.onmicrosoft.com/509b5353-3a44-4d09-85ed-64bf2e30ccb8/rideshare'];
+window.apiScopes = ['https://wviriyab2c.onmicrosoft.com/509b5353-3a44-4d09-85ed-64bf2e30ccb8/rideshare'];
 window.authEnabled = true;
 
 
