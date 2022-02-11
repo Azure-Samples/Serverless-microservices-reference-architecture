69d46290e8fb15c2db14158d7619e966ed808fc8
Daniel Larsen <dalars@microsoft.com>
2022-02-11 16:54:21 +1300

    run local works, test local works, not isolated yet

--- scripts/run-local.ps1
+++ scripts/run-local.ps1
@@ -1,5 +1,5 @@
 # NOTE: Install latest versions of Node and NPM: https://nodejs.org/en/download/
-# NOTE: Install latest version of func CLI: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local
+# NOTE: Function core tools v4 must be installed: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v4%2Cwindows%2Ccsharp%2Cportal%2Cbash#install-the-azure-functions-core-tools
 # NOTE: Start Azure Storage Emulator https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator
 
 $ErrorActionPreference = 'Stop'
@@ -19,17 +19,20 @@ try {
     Write-Host 'Build and start Trip Archiver Nodejs Function...'
     Start-Process pwsh { -c cd ../nodejs/serverless-microservices-functionapp-triparchiver && npm install && npm run pack && func start --javascript -p 7075 --cors http://localhost:4280 }
 
+    Write-Host 'Build dotnet...'
+    dotnet build -c Debug
+
     Write-Host 'Build and start Drivers Function...'
-    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Drivers && func start --csharp -p 7071 --cors http://localhost:4280 }
+    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Drivers/bin/Debug/net6.0 && func start --csharp -p 7071 --cors http://localhost:4280 --no-build }
     
     Write-Host 'Build and start Trips Function...'
-    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Trips && func start --csharp -p 7072 --cors http://localhost:4280 }
+    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Trips/bin/Debug/net6.0 && func start --csharp -p 7072 --cors http://localhost:4280 --no-build }
     
     Write-Host 'Build and start Passengers Function...'
-    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Passengers && func start --csharp -p 7073 --cors http://localhost:4280 }
+    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Passengers/bin/Debug/net6.0 && func start --csharp -p 7073 --cors http://localhost:4280 --no-build }
     
     Write-Host 'Build and start Orchestrators Function...'
-    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Orchestrators && func start --csharp -p 7074 --cors http://localhost:4280 }
+    Start-Process pwsh { -c cd ServerlessMicroservices.FunctionApp.Orchestrators/bin/Debug/net6.0 && func start --csharp -p 7074 --cors http://localhost:4280 --no-build }
 
     Write-Host 'When all builds have finished, open browser at http://localhost:4280/'
 
